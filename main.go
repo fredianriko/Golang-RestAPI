65702f90a36e96a8f76e7320f47da98da2901124
Fredi Anriko <fredianriko@gmail.com>
2022-07-05 22:01:34 +0700

    complete updateTodo and deleteTodo

--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 //import depedencies
 import (
-	"io/ioutil"//package to read request body
+	// "encoding/json"
+	"encoding/json"
+	"errors"
+
 	"net/http"
 	"github.com/gin-gonic/gin"
 )
@@ -25,9 +28,7 @@ var todos = []todo{
 
 //controller GET
 func getTodos(context *gin.Context) {
-
 	context.IndentedJSON(http.StatusOK, todos)
-
 }
 
 //controller POST
@@ -35,8 +36,7 @@ func addTodo(context *gin.Context){
 	var newTodo todo
 
 	//read request body
-	body, _ := ioutil.ReadAll(context.Request.Body)
-    println(string(body))
+	// title := json.Unmarshal(newTodo)
 
 
 	//equal with JSON.parse(context.Request.Body)
@@ -52,24 +52,81 @@ func addTodo(context *gin.Context){
 	context.IndentedJSON(http.StatusCreated, newTodo)
 }
 
-func getTodoById(context *gin.Context){
+func getTodoById(id string) (*todo, error){
+	for i, t := range todos{
+		if t.ID == id {
+			return &todos[i], nil
+		}
+	}
+	return nil, errors.New("todo not found")
+}
 
-	//get request param 
+func getTodo(context *gin.Context){
+	id := context.Param("id")
+	todo, err := getTodoById(id)
+
+	if err != nil{
+		context.IndentedJSON(http.StatusNotFound, gin.H{"message": "todo not found"})
+	}
+
+	context.IndentedJSON(http.StatusOK, todo)
+}
+
+func updateTodo(context *gin.Context){
+	
+	//decode the request body json and store it to decoder variabel
+ 	decoder := json.NewDecoder(context.Request.Body)
+
+	//create new variabel that has the type of todo
+	var t todo
+
+	//if error value is nil, then decode the request body json and store it to t variabel
+	err := decoder.Decode(&t)
+
+	//if error value exist, stop all process and return error
+	if err != nil {
+        panic(err)
+    }
+
+	//get one todo from todos array where id = t.ID
+	todo, err := getTodoById(t.ID)
+
+	//if error value exist, stop all process and return error
+	if err != nil{
+		context.IndentedJSON(http.StatusNotFound, gin.H{"message": "todo not found"})
+	}
+
+	//assign new value to todo with value from request body
+	todo.Item = t.Item
+	todo.Completed = t.Completed
+
+	//return the updated todo with json format and status response
+	context.IndentedJSON(http.StatusOK, todo)
+}
+
+func deleteTodo(context *gin.Context){
 	id := context.Param("id")
 
+	//get one todo from todos array where id = id
+	_, err := getTodoById(id)
 
 
-	//loop over todos, equals to forEach in javascript
-	for _, item := range todos {
-		//when meet the id, return the todos
-		if item.ID == id {
-			context.IndentedJSON(http.StatusOK, item)
-			return
+	//if no data found with id, return error
+	if err != nil{
+		context.IndentedJSON(http.StatusNotFound, gin.H{"message": "todo not found"})
+	}
+
+	//iterate ofer todos slice, delete todo from todos array where todo.ID = id
+	for i, t := range todos{
+		if t.ID == id {
+			todos = append(todos[:i], todos[i+1:]...)
 		}
 	}
 
-	//return status 404 if id is not found
-	context.Status(http.StatusNotFound)
+	
+	//return the deleted todo with json format and status response
+	context.IndentedJSON(http.StatusOK, todos)
+	
 }
 
 
@@ -79,14 +136,20 @@ func main(){
 	//make new gin server instance
 	router := gin.Default()
 
-	//routes GET
+	//getAllTodos
 	router.GET("/todos", getTodos)
 
-	//route get 1 todo
-	router.GET("/todos/:id", getTodoById)
+	//getOneTodo
+	router.GET("/todos/:id", getTodo)
 
-	//routes POST
+	//postNewTodo
 	router.POST("/todos", addTodo)
+
+	//updateTodo	
+	router.PATCH("/todos", updateTodo)
+
+	//deleteTodo
+	router.DELETE("/todos/:id", deleteTodo)
 
 
 	//start server
